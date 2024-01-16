09a34c183926fe4d9da125fc0609eca20bd982de
M. Ali Imron <lienux.qq@gmail.com>
2024-01-16 14:37:42 +0700

    update

--- src/Master.php
+++ src/Master.php
@@ -17,7 +17,6 @@ class Master
         $url = "api/master/site_project/show";
         $method = "GET";
         $module_code = "site_project";
-        $token = null;
 
         $create = $this->curli->request($url,$method,$payload,$module_code,$token);
 
