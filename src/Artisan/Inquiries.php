bd5ab7749b8792c55b5a7f7624661033f766feb6
M. Ali Imron <lienux.qq@gmail.com>
2024-01-26 16:48:57 +0700

    create curl for artisan

---
+++ src/Artisan/Inquiries.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dorbitt\Artisan;
+
+use Dorbitt\Artisan\Curl;
+
+class Inquiries
+{
+    public static function show($id = null, $payload = null, $token = null)
+    {
+        if ($id) {
+            $path = "api/crm/inquiries/show/" . $id;
+        }else{
+            $path = "api/crm/inquiries/show";
+        }
+
+        $params = [
+            "path"           => $path,
+            "method"         => "GET",
+            "payload"        => $payload,
+            "module_code"    => "crm_inquiries",
+            "token"          => $token
+        ];
+
+        $response = Curl::request($params);
+
+        return json_decode($response, false);
+    }
+
+    public static function create($payload, $token = null)
+    {
+        $params = [
+            "path"           => "api/crm/inquiries/create",
+            "method"         => "POST",
+            "payload"        => $payload,
+            "module_code"    => "crm_inquiries",
+            "token"          => $token
+        ];
+
+        $response = Curl::request($params);
+
+        return json_decode($response, false);
+    }
+}
