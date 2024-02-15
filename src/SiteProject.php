b3b7a7814049409076b3c4d3660a4c2461fd3b88
M. Ali Imron <lienux.qq@gmail.com>
2024-02-15 10:37:18 +0700

    update

--- src/SiteProject.php
+++ src/SiteProject.php
@@ -24,6 +24,7 @@ class SiteProject
         }
 
         $params = [
+            "url"            => $path,
             "path"           => $path,
             "method"         => "GET",
             "payload"        => $payload,
