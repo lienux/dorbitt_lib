26c66d6f39c668f3bf9b39f09c0fb36793f4c0a9
M. Ali Imron <lienux.qq@gmail.com>
2024-02-21 09:04:49 +0700

    update curl

--- src/Auth.php
+++ src/Auth.php
@@ -11,15 +11,20 @@ class Auth
         $this->curli = new Curl();
     }
 
-    public function login($payload = null, $token = null)
+    public function login($payload)
     {
-        $url = "auth/login/create";
-        $method = "GET";
-        $module_code = "";
+        $params = [
+            "path"           => "auth/login/create",
+            "method"         => "POST",
+            "payload"        => $payload,
+            "module_code"    => null,
+            "token"          => null
+        ];
 
-        $create = $this->curli->request($url,$method,$payload,$module_code,$token);
+        $response = $this->curli->request3($params);
 
-        return json_decode($create, false);
+        return json_decode($response, false);
+        // return $response;
 
     }
 
--- src/Curl.php
+++ src/Curl.php
@@ -6,8 +6,10 @@ class Curl
 {
     public function __construct()
     {
-        if (getenv("CI_DORBITT")=="testing") {
+        if (getenv("CI_DORBITT")=="development") {
             $this->url = "http://localhost:8080/";
+        }elseif (getenv("CI_DORBITT")=="testing") {
+            $this->url = "http://testing-api.dorbitt.com/";
         }else{
             $this->url = "https://api.dorbitt.com/";
         }
@@ -17,6 +19,18 @@ class Curl
         }else{
             $this->token = "";
         }
+    }
+
+    public function valcurl()
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$this->url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        $curlresult=curl_exec ($ch);
+        curl_close ($ch);
+        
+        if (!preg_match("/OK/i", $curlresult))
+        return "The curl action has FAILED! (OUTPUT of curl is: ".$curlresult."), please check your internet connection";
     }
 
     public function request($url,$method,$payload,$module_code,$token = null)
@@ -91,6 +105,51 @@ class Curl
         curl_close($curl);
 
         return $response;
+
+    }
+
+    public function request3($params)
+    {
+        $path           = $params['path'];
+        $method         = $params['method'];
+        $payload        = $params['payload'];
+        $module_code    = $params['module_code'];
+        $token          = $params['token'];
+
+        if ($token == null) {
+            $token = $this->token;
+        }elseif ($token == 'session') {
+            $token = session()->get('token');
+        }
+
+        $url_ = $this->url . $path;
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url_,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Module-Code: '. $module_code,
+                'Authorization: Bearer ' . $token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $response;
+
+        // return $url_;
 
     }
 }
