59089c081f4d85154b7d2f1d295f8fbc80ab08cd
M. Ali Imron <lienux.qq@gmail.com>
2024-02-22 14:02:43 +0700

    add function ummu on curl

--- src/Curl.php
+++ src/Curl.php
@@ -152,4 +152,33 @@ class Curl
         // return $url_;
 
     }
+
+    public function ummu($params)
+    {
+        $url            = $params['url'];
+        $method         = $params['method'];
+        $payload        = $params['payload'];
+        $headers        = $params['headers'];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => json_encode($payload),
+            CURLOPT_HTTPHEADER => $headers,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_decode($response, false);
+    }
 }
