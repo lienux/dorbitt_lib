42cb3248bd93b5ac9b6b3121d395ca2c34160fd3
M. Ali Imron <lienux.qq@gmail.com>
2024-06-04 21:36:08 +0700

    update logs

--- src/UmmuLogs.php
+++ src/UmmuLogs.php
@@ -41,4 +41,29 @@ class UmmuLogs
         fwrite($fp, "<p>" . $label . " " . date("Y-m-d H:i:s") . "<br>" . "\n");
         fwrite($fp, json_encode($text) . "<br>" . "\n");
     }
+
+    public function create2($filename, $text, $subject = null)
+    {
+        $filename = $filename. '_'. date("Y-m-d") . '.html';
+
+        if ($subject) {
+            $subject = $subject . ' | ';
+        }
+
+        if (! is_dir("/var/www/html/ummuLogs")) {
+            exec("mkdir /var/www/html/ummuLogs");
+        }
+
+        exec("chmod -R 777 /var/www/html/ummuLogs");
+
+        if (! is_file("/var/www/html/ummuLogs/" . $filename)) {
+            exec("touch /var/www/html/ummuLogs/" . $filename);
+        }
+
+        exec("chmod -R 777 /var/www/html/ummuLogs");
+
+        $fp = fopen('/var/www/html/ummuLogs/' . $filename, 'a');
+        fwrite($fp, "<p>" . $subject . date("Y-m-d H:i:s") . "<br>" . "\n");
+        fwrite($fp, json_encode($text) . "<br>" . "\n");
+    }
 }
