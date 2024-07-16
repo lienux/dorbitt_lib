244ccc63d9684fd6c150323afd3fbef82a898793
M. Ali Imron <lienux.qq@gmail.com>
2024-07-16 11:07:03 +0700

    update

---
+++ src/UmmuRef.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Dorbitt;
+
+/**
+* =============================================
+* Author: Ummu
+* Website: https://ummukhairiyahyusna.com/
+* App: DORBITT LIB
+* Description: 
+* =============================================
+*/
+
+use Dorbitt\Curl;
+
+class UmmuRef
+{
+    public function __construct()
+    {
+        $this->curli = new Curl();
+    }
+
+    public function show($params)
+    {
+        $id = $params['id'];
+        $payload = $params['payload'];
+        $token = $params['token'];
+        
+        if ($id) {
+            $path = "api/referensi/getAll/" . $id;
+        }else{
+            $path = "api/referensi/getAll";
+        }
+
+        $params = [
+            "path"           => $path,
+            "method"         => "GET",
+            "payload"        => $payload,
+            "module_code"    => "referensi",
+            "token"          => $token
+        ];
+
+        $response = $this->curli->request3($params);
+
+        return json_decode($response, false);
+    }
+}
