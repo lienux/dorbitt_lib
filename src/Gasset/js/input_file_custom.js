13ec24f565dd9d373c56e1d53be85e6dddfde3e7
M. Ali Imron <lienux.qq@gmail.com>
2025-06-09 02:27:02 +0700

    after clear cached .

--- src/Gasset/js/input_file_custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    bsCustomFileInput.init()
-
-    var btn = document.getElementById('btnResetForm')
-    var form = document.querySelector('form')
-    btn.addEventListener('click', function() {
-        form.reset()
-    })
-});
