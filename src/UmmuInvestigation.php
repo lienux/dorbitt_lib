6c082f3e5619377509b8ef738e9faed7fe61f317
M. Ali Imron <lienux.qq@gmail.com>
2025-07-16 13:29:49 +0700

    update

---
+++ src/UmmuInvestigation.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Dorbitt;
+
+/**
+* =============================================
+* Author: Ummu
+* Website: https://ummukhairiyahyusna.com/
+* App: DORBITT LIB
+* Description: 
+* =============================================
+*/
+
+use Dorbitt\Curl;
+use Dorbitt\GlobalHelper;
+
+class UmmuInvestigation
+{
+    public function __construct()
+    {
+        $this->curli = new Curl();
+        $this->gHelp = new GlobalHelper();
+        $this->urli = 'api/she/investigation/';
+    }
+
+    public function show_created_name($params)
+    {
+        $response = $this->curli->request4(
+            [
+                "path"           => $this->urli . "show_created_name/".$params["id"],
+                "method"         => "GET",
+                "payload"        => $params['payload'],
+                "module_code"    => "she_investigation",
+                "token"          => $params['token']
+            ]
+        );
+
+        return json_decode($response, false);
+    }
+
+    // public function show($params)
+    // {
+    //     $response = $this->curli->request4(
+    //         [
+    //             "path"           => $this->urli . "show",
+    //             "method"         => "GET",
+    //             "payload"        => $params['payload'],
+    //             "module_code"    => "she_hazard_report",
+    //             "token"          => $params['token']
+    //         ]
+    //     );
+
+    //     return json_decode($response, false);
+    // }
+
+    // public function insert($params)
+    // {
+    //     $response = $this->curli->request4([
+    //         "path"           => $this->urli. "create",
+    //         "method"         => "POST",
+    //         "payload"        => $params["payload"],
+    //         "module_code"    => "she_hazard_report",
+    //         "token"          => $params["token"]
+    //     ]);
+
+    //     return json_decode($response, false);
+    // }
+
+    // public function delete($params)
+    // {
+    //     $response = $this->curli->request4(
+    //         [
+    //             "path"           => $this->urli. "delete",
+    //             "method"         => "DELETE",
+    //             "payload"        => $params['payload'],
+    //             "module_code"    => "she_hazard_report",
+    //             "token"          => $params['token']
+    //         ]
+    //     );
+
+    //     return json_decode($response, false);
+    // }
+
+    // public function update($params)
+    // {
+    //     $response = $this->curli->request4(
+    //         [
+    //             "path"           => $this->urli. "update",
+    //             "method"         => "PUT",
+    //             "payload"        => $params['payload'],
+    //             "module_code"    => "she_hazard_report",
+    //             "token"          => $params['token']
+    //         ]
+    //     );
+
+    //     return json_decode($response, false);
+    // }
+
+    // public function release($params)
+    // {
+    //     $response = $this->curli->request4(
+    //         [
+    //             "path"           => $this->urli. "release",
+    //             "method"         => "POST",
+    //             "payload"        => $params['payload'],
+    //             "module_code"    => "she_hazard_report",
+    //             "token"          => $params['token']
+    //         ]
+    //     );
+
+    //     return json_decode($response, false);
+    // }
+}
