6c3c42852ab363053775eddc06db5e0047af2eb4
M. Ali Imron <lienux.qq@gmail.com>
2026-01-22 17:31:11 +0700

    update

---
+++ src/Views/partials/summary_daily_production.php
@@ -0,0 +1,23 @@
+<div class="col-lg-6 col-md-6 col-sm-12 mb-3 px-0">
+    <div class="col-lg-12 col-md-12 col-sm-12 pl-0 mb-1">
+        <label class="text-info text-sm m-0">Date <span class="text-danger">*</span></label>
+        <div class="input-group input-group-sm">
+            <div class="input-group-prepend">
+                <span class="input-group-text">From</span>
+            </div>
+            <input type="text" class="form-control ummu-datepicker ummu-datepicker-default-from" id="iDateFrom" disabled>
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary btn-show-datepicker" type="button"
+                    data-inputid="iDateFrom"><i class="fas fa-calendar-alt"></i></button>
+            </div>
+            <div class="input-group-append ml-2">
+                <button class="btn btn-primary rounded" type="button"
+                    id="btn_get_data"><i class="fas fa-file-search"></i> Retrieve</button>
+            </div>
+            <div class="input-group-append ml-2">
+                <button class="btn btn-primary rounded" type="button"
+                    id="btn_print_to_pdf_daily_meeting"><i class="fas fa-file-pdf"></i> PDF Daily Meeting</button>
+            </div>
+        </div>
+    </div>
+</div>
