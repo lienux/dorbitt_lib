94fc6c7a0cfc8b22ebfe487285bf00bb56fcd924
Ali Programmer Hillcon <ali.imong.programmer@gmail.com>
2026-02-03 21:20:28 +0700

    update ummu.js

---
+++ src/Views/partials/btn_col_size.php
@@ -0,0 +1,13 @@
+<div class="col-lg-4 col-md-4 col-sm-12 pl-0 mb-1">
+    <div class="input-group input-group-sm">
+        <div class="input-group-append">
+            <button class="btn btn-secondary rounded btn_col_size_3" type="button" id=""><i class="fas fa-columns"></i>-3</button>
+        </div>
+        <div class="input-group-append ml-2">
+            <button class="btn btn-secondary rounded btn_col_size_2" type="button" id=""><i class="fas fa-columns"></i>-2</button>
+        </div>
+        <div class="input-group-append ml-2">
+            <button class="btn btn-secondary rounded btn_col_size_1" type="button" id=""><i class="fas fa-columns"></i>-1</button>
+        </div>
+    </div>
+</div>
