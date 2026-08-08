0b3476b0c15b11f030a4a78b80a5298de63e515b
Ali Programmer Hillcon <ali.imong.programmer@gmail.com>
2026-08-08 20:01:41 +0700

    update

--- src/Views/partials/sbadmin2/topbar.php
+++ src/Views/partials/sbadmin2/topbar.php
@@ -38,9 +38,9 @@
 
 <!-- Sidebar Toggle (Topbar) -->
 <?php if ($login_module == 'mcp') { ?>
-<nav class="navbar navbar-expand topbar static-top" style="height: 3rem; background-color: rgba(0, 0, 0, 1) !important;">
+<nav class="navbar navbar-expand topbar static-top" style="background-color: rgba(0, 0, 0, 1) !important;">
 <?php } else { ?>
-<nav class="navbar navbar-expand navbar-lightz bg-whitez bg-primaryz topbar static-top" style="height: 3rem; background-color: #350059 !important;">
+<nav class="navbar navbar-expand topbar static-top">
 <?php } ?>
     <!-- <button id="sidebarToggleTop" class="btn btn-link d-md-nonez rounded-circle mr-3">
         <i class="fa fa-bars"></i>
@@ -50,25 +50,73 @@
         <span class="d-block d-sm-none"><i class="bi bi-grid-fill"></i></span>
     </button>
 
-    <!-- Topbar Search -->
-    <!-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-    <div class="input-group">
-        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-            aria-label="Search" aria-describedby="basic-addon2">
-        <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-                <i class="fas fa-search fa-sm"></i>
-            </button>
-        </div>
-    </div>
-    </form> -->
+    <div class="h5 text-light my-0 collapse"><?= (isset($page_title) ? $page_title : '') ?></div>
 
-    <div class="h5 text-light my-0"><?= (isset($page_title) ? $page_title : '') ?></div>
+    <!-- Menu module yang terkait -->
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbars02" aria-controls="navbars02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="navbars02">
+        <ul class="navbar-nav mr-auto text-light">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-home d-inline d-lg-none"></i>
+                    <span class="d-none d-lg-inline ml-2">Home</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="fas fa-link d-inline d-lg-none"></i>
+                    <span class="d-none d-lg-inline ml-2">Link</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">
+                    <i class="fas fa-shopping-cart d-inline d-lg-none"></i>
+                    <span class="d-none d-lg-inline ml-2">Disabled</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-caret-square-down d-inline d-lg-none"></i>
+                    <span class="d-none d-lg-inline ml-2">Dropdown</span>
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link dropdown-toggle" href="#" id="customDropdown" role="button"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-cog fa-fw d-inline d-lg-none"></i>
+                    <span class="d-none d-lg-inline ml-2">Pengaturan</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                    aria-labelledby="customDropdown">
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-sliders-h fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Opsi A
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-tools fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Opsi B
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">
+                        <i class="fas fa-question-circle fa-sm fa-fw mr-2 text-gray-400"></i>
+                        Bantuan
+                    </a>
+                </div>
+            </li>
+        </ul>
+    </div>
 
     <!-- Topbar Navbar -->
-    <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav ml-auto text-light">
         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-        <li class="nav-item dropdown no-arrow d-sm-none">
+        <li class="nav-item dropdown no-arrow d-sm-none collapse">
             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-search fa-fw"></i>
@@ -91,7 +139,8 @@
         </li>
 
         <!-- Notification -->
-        <!-- include('bocahgantengdotcom.partials.notification') -->
+        <?= $this->include(config('Vh')->ummuView('partials/' . config('Vh')->tmp() . '/sidebar_alert')) ?>
+        <?= $this->include(config('Vh')->ummuView('partials/' . config('Vh')->tmp() . '/sidebar_msg')) ?>
 
         <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -99,7 +148,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-light small"><?= $nameV ?></span>
+                <span class="mr-2 d-none d-lg-inline small"><?= $nameV ?></span>
                 <img class="img-profile rounded-circle" src="<?= $vH->avatar() ?>">
             </a>
             <!-- Dropdown - User Information -->
